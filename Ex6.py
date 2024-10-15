741f4a2f2495a76eb9082da7f44888705c41287a
laurent.sauzet <laurent.sauzet@cgi.com>
2024-10-15 16:44:26 +0200

    Les Variables

---
+++ Ex6.py
@@ -0,0 +1,7 @@
+a = 5
+_a = 5
+nombre_impair = 3
+nombre_pair = 2
+a_ = 5
+a_3 = 6
+a_2_b = 8
---
+++ ex4.py
@@ -0,0 +1,4 @@
+prenom = "Pierre"              
+age = 20                
+majeur = True                  
+compte_en_banque = 20135.384  
