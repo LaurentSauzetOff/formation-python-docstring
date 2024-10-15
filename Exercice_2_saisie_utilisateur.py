7b971d1aafe28a8ff648c375b2d29459e01cc22d
laurent.sauzet <laurent.sauzet@cgi.com>
2024-10-15 17:18:02 +0200

    Interactions basiques avec l'utilisateur

---
+++ Exercice_2_saisie_utilisateur.py
@@ -0,0 +1,7 @@
+prenom = input("Veuillez entrer votre prénom : ")
+ville = input ("Veuillez préciser votre ville : ")
+age = input("Veuillez entrer votre age : ")
+
+print("Bonjour, " + prenom + "!")
+print("Vous êtes né(e) à " + ville + ".")
+print("Vous avez " + age + " ans.")
