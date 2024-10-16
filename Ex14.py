8ec68d346fd7d0067dfcdf96d47a187c650f4558
laurent.sauzet <laurent.sauzet@cgi.com>
2024-10-16 17:04:33 +0200

    little modifs

--- Ex14.py
+++
@@ -1,9 +0,0 @@
-# Ne modifiez pas les variables ci-dessous
-protocole = "https://"
-nom_du_site = "docstring"
-extension = "fr"
-page = "glossaire"
-
-# Modifiez le code à partir d'ici
-URL = f"{protocole}www.{nom_du_site}.{extension}/{page}/"
-print(URL)
